3e853a3132427f25853ead4806a429a7140b3a31
Marllon Gomes <marllon.fgomes@gmail.com>
2023-07-02 18:03:20 -0300

    feat: create btn in groups screen

--- ignite-teams/src/screens/Groups/index.tsx
+++ ignite-teams/src/screens/Groups/index.tsx
@@ -1,30 +1,29 @@
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { GroupCard } from '@components/GroupCard';
-import * as S from './styles';
-import { useState } from 'react';
-import { FlatList } from 'react-native';
-import { ListEmpty } from '../../components/ListEmpty';
+import { Button } from "@components/Button";
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import * as S from "./styles";
 
 export function Groups() {
-
   const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <S.Container>
       <Header />
-      <Highlight 
-        title="Turmas"
-        subtitle="Jogue com a sua turma"
-      />
-      <FlatList 
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <FlatList
         data={groups}
-        keyExtractor={item => item}
+        keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
-        ListEmptyComponent={() => <ListEmpty message="Nenhuma turma encontrada" />}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhuma turma encontrada" />
+        )}
       />
+      <Button title="Criar nova turma" />
     </S.Container>
   );
 }
-
