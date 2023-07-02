0c3f34be0b2c476a6efcc4a69b66a116b86a4161
Marllon Gomes <marllon.fgomes@gmail.com>
2023-07-02 17:52:50 -0300

    feat: update in screen show list of teams

--- ignite-teams/src/screens/Groups/index.tsx
+++ ignite-teams/src/screens/Groups/index.tsx
@@ -1,10 +1,29 @@
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 import * as S from './styles';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '../../components/ListEmpty';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <S.Container>
       <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Nenhuma turma encontrada" />}
+      />
     </S.Container>
   );
 }
