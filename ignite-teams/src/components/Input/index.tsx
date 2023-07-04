b4f02cf801743e3fa3b194fd6d0fe58eb9e8d237
Marllon Gomes <marllon.fgomes@gmail.com>
2023-07-03 21:15:26 -0300

    feat: add input comp

---
+++ ignite-teams/src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+import { TextInputProps } from 'react-native';
+
+export function Input({...rest}: TextInputProps) {
+  return (
+    <S.Container {...rest} />
+  );
+}
+
