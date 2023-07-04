2ecd97486b68b3fee0b7e6a0313659493f15a4a6
Marllon Gomes <marllon.fgomes@gmail.com>
2023-07-04 09:25:25 -0300

    refactor: imports of app.tsx

--- ignite-teams/App.tsx
+++ ignite-teams/App.tsx
@@ -1,26 +1,17 @@
 import { Loading } from "@components/Loading";
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from "@expo-google-fonts/roboto";
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import { StatusBar } from "expo-status-bar";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Players /> : <Loading />}
-      <StatusBar 
-        style="light"
-        translucent        
-      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+      <StatusBar style="light" translucent />
     </ThemeProvider>
   );
 }
